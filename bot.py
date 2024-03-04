7bf4f1fb4873286df06954ac817139462fb1b897
Vildan <vildan@gmail.com>
2024-03-04 10:38:27 +0500

    2 ta handler yozdik

--- bot.py
+++ bot.py
@@ -1,2 +1,17 @@
 # Aiogram
 # pyTelegramBotApi
+
+# -----------------------------------------------------------------------
+
+from telebot import TeleBot
+from telebot.types import Message
+bot = TeleBot(token='6948819597:AAEn1SRyai2hNC4gOyHpYMEp-fbmg9jOulI')
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message: Message):
+    chat_id = message.chat.id
+    full_name = message.from_user.full_name
+    bot.send_message(chat_id, f"Assalomu Alaykum {full_name}")
+
+
+bot.polling()
